b33fcc96b12be3526758b67154c6d44a26498f63
Verdysh <verdysh1991@gmail.com>
2020-07-07 17:39:46 +0300

    Fix commit

--- src/main/java/com/ua/verdysh/Main.java
+++ src/main/java/com/ua/verdysh/Main.java
@@ -28,21 +28,3 @@ public class Main {
         }
     }
 }
-
-
-
-// basic profiles
-// tbody > tr td:nth-child(1) name
-// tbody > tr td:nth-child(2) phone
-// tbody > tr td:nth-child(3) mail
-
-// administration
-// tbody > tr td:nth-child(1) name
-// tbody > tr td:nth-child(2) job title
-// tbody > tr td:nth-child(3) phone
-// tbody > tr td:nth-child(4) mail
-
-// fullprofile
-// ul.small-block-grid-2.medium-block-grid-4.large-block-grid-5 div.attorney-card > a   attorneys url
-
-// ul.small-block-grid-2.medium-block-grid-4.large-block-grid-5 a   emeritus url
